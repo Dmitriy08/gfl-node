f6103e7a6ba92d46f070e2c04a380901f441eaef
Dmitriy Pilyavskiy <dmitriy.pilyavskiy@gmail.com>
2021-05-27 16:54:16 +0300

    fix folder render

--- models/File.js
+++ models/File.js
@@ -2,126 +2,126 @@ const fs = require("fs");
 const path = require("path");
 
 class FileApp {
-	USER_MAX_SIZE = 12428800 / 2;
-	USED_MEMORY = 0;
+    USER_MAX_SIZE = 12428800 / 2;
+    USED_MEMORY = 0;
 
-	createUserDir(username) {
-		if (!fs.existsSync(upload_dir + '/' + username)) {
-			fs.mkdirSync(upload_dir + '/' + username);
-		}
-	}
+    createUserDir(username) {
+        if (!fs.existsSync(upload_dir + '/' + username)) {
+            fs.mkdirSync(upload_dir + '/' + username);
+        }
+    }
 
-	__getFolderSize(dirTree) {
-		let size = 0;
-		dirTree.forEach(item => {
-			size += item.sizeBytes
-		})
-		return size;
-	};
+    __getFolderSize(dirTree) {
+        let size = 0;
+        dirTree.forEach(item => {
+            size += item.sizeBytes
+        })
+        return size;
+    };
 
-	__formatSizeUnits(bytes) {
-		if (bytes >= 1073741824) {
-			bytes = (bytes / 1073741824).toFixed(2) + " GB";
-		} else if (bytes >= 1048576) {
-			bytes = (bytes / 1048576).toFixed(2) + " MB";
-		} else if (bytes >= 1024) {
-			bytes = (bytes / 1024).toFixed(2) + " KB";
-		} else if (bytes > 1) {
-			bytes = bytes + " bytes";
-		} else if (bytes == 1) {
-			bytes = bytes + " byte";
-		} else {
-			bytes = "0 bytes";
-		}
-		return bytes;
-	}
+    __formatSizeUnits(bytes) {
+        if (bytes >= 1073741824) {
+            bytes = (bytes / 1073741824).toFixed(2) + " GB";
+        } else if (bytes >= 1048576) {
+            bytes = (bytes / 1048576).toFixed(2) + " MB";
+        } else if (bytes >= 1024) {
+            bytes = (bytes / 1024).toFixed(2) + " KB";
+        } else if (bytes > 1) {
+            bytes = bytes + " bytes";
+        } else if (bytes == 1) {
+            bytes = bytes + " byte";
+        } else {
+            bytes = "0 bytes";
+        }
+        return bytes;
+    }
 
-	getFolderItems(pathName, username) {
-		const res = [];
-		try {
-			const dirItems = fs.readdirSync(pathName);
-			dirItems.forEach((item, i) => {
-				try {
-					const {basename: base, dir} = path.parse(path.join(pathName, item));
-					const stats = fs.statSync(path.join(pathName, item));
-					let id = Math.floor(Math.random() * 10000000) + i
-					let extname = path.extname(item)
-					let isImg = false;
-					if (extname === '.jpg' || extname === '.png') {
-						isImg = true
-					}
-					if (stats.isFile()) {
-						res.push({
-							id: id,
-							dirname: __dirname,
-							name: item,
-							src: '/' + pathName + '/' + item,
-							isImg,
-							basename: base,
-							dir,
-							sizeBytes: stats.size,
-							size: this.__formatSizeUnits(stats.size),
-							birthtime: new Date(stats.ctime).toLocaleDateString(),
-							isFile: stats.isFile(),
-							isDir: stats.isDirectory(),
-						});
-					} else {
-						res.push({
-							id: id,
-							name: item,
-							basename: base,
-							dir,
-							sizeBytes: stats.size,
-							size: this.__formatSizeUnits(stats.size),
-							birthtime: new Date(stats.ctime).toLocaleDateString(),
-							isFile: stats.isFile(),
-							isDir: stats.isDirectory(),
-							items: this.getFolderItems(path.join(pathName, item)),
-						});
-					}
-				} catch (err) {
-					console.log(err);
-				}
-			});
-			return res;
-		} catch (err) {
-			console.log('ERROR', err);
-			return res;
-		}
-	};
+    getFolderItems(pathName, username) {
+        const res = [];
+        try {
+            const dirItems = fs.readdirSync(pathName);
+            dirItems.forEach((item, i) => {
+                try {
+                    const {basename: base, dir} = path.parse(path.join(pathName, item));
+                    const stats = fs.statSync(path.join(pathName, item));
+                    let id = Math.floor(Math.random() * 10000000) + i
+                    let extname = path.extname(item)
+                    let isImg = false;
+                    if (extname === '.jpg' || extname === '.png') {
+                        isImg = true
+                    }
+                    if (stats.isFile()) {
+                        res.push({
+                            id: id,
+                            dirname: __dirname,
+                            name: item,
+                            src: '/' + pathName + '/' + item,
+                            isImg,
+                            basename: base,
+                            dir,
+                            sizeBytes: stats.size,
+                            size: this.__formatSizeUnits(stats.size),
+                            birthtime: new Date(stats.ctime).toLocaleDateString(),
+                            isFile: stats.isFile(),
+                            isDir: stats.isDirectory(),
+                        });
+                    } else {
+                        res.push({
+                            id: id,
+                            name: item,
+                            basename: base,
+                            dir,
+                            sizeBytes: stats.size,
+                            size: this.__formatSizeUnits(stats.size),
+                            birthtime: new Date(stats.ctime).toLocaleDateString(),
+                            isFile: stats.isFile(),
+                            isDir: stats.isDirectory(),
+                            items: this.getFolderItems(path.join(pathName, item)),
+                        });
+                    }
+                } catch (err) {
+                    console.log(err);
+                }
+            });
+            return res;
+        } catch (err) {
+            console.log('ERROR', err);
+            return res;
+        }
+    };
 
-	get AllMemory() {
-		return this.USER_MAX_SIZE
-	}
+    get AllMemory() {
+        return this.USER_MAX_SIZE
+    }
 
-	get UsedMemory() {
-		return this.USED_MEMORY
-	}
+    get UsedMemory() {
+        return this.USED_MEMORY
+    }
 
 
-	addFiles(filesList, username) {
-		filesList.forEach((file, i) => {
-			file.mv(upload_dir + `/${username}/` + file.name, (err) => {
-					const result = {
-						id: i,
-						name: file.name,
-						mimetype: file.mimetype,
-						size: file.size,
-						status: true,
-					};
-				}
-			);
-		});
-	}
+    addFiles(filesList, username) {
+         filesList.forEach((file, i) => {
+            file.mv(upload_dir + `/${username}/` + file.name, (err) => {
+                    const result = {
+                        id: i,
+                        name: file.name,
+                        mimetype: file.mimetype,
+                        size: file.size,
+                        status: true,
+                    };
+                }
+            );
+        });
+    }
 
-	getMemory(pathname) {
-		this.USED_MEMORY = this.__getFolderSize(pathname);
-		return {
-			allMemory: this.__formatSizeUnits(this.USER_MAX_SIZE),
-			freeMemory: this.__formatSizeUnits(this.USER_MAX_SIZE - this.USED_MEMORY),
-			usedMemory: this.__formatSizeUnits(this.USED_MEMORY)
-		};
-	}
+    getMemory(pathname) {
+        this.USED_MEMORY = this.__getFolderSize(pathname);
+        return {
+            allMemory: this.__formatSizeUnits(this.USER_MAX_SIZE),
+            freeMemory: this.__formatSizeUnits(this.USER_MAX_SIZE - this.USED_MEMORY),
+            usedMemory: this.__formatSizeUnits(this.USED_MEMORY)
+        };
+    }
 }
 
 module.exports = new FileApp();
--- public/js/front.js
+++ public/js/front.js
@@ -1,132 +1,131 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const username = window.location.pathname;
 	const table = document.querySelector('.table')
-	let tableBody = document.querySelector('.table tbody');
-	let memoryWrap = document.querySelector('.space');
-	let infoUl = document.querySelector('.information-content ul');
-	let infoImg = document.querySelector('.information-media');
-	let infoDownload = document.querySelector('.information-download');
-	let state = [];
-	let stateInner = [];
-	let idDir;
-
-	const makeRender = (selector) => {
-		let template = document.querySelector(selector).innerHTML;
-		return new Function('data', 'return `' + template + '`');
-	}
-
-	const renderFileSystemMemory = (memory) => {
-		memoryWrap.innerHTML = '';
-		let listRender = makeRender('.freeSpaceTemplate');
-		memoryWrap.innerHTML = listRender(memory);
-	}
-
-	const renderTableBody = (userFiles, dir = false, up = false) => {
-		tableBody.innerHTML = '';
-		let listRender = makeRender('.tableBody');
-		let template = userFiles.map((data) => listRender(data))
-		if (up) {
-			stateInner.slice(0, -1)
-		}
-		if (dir) {
-			let templateDirUp = `
-				<tr data-up>
-					<td colspan="4">...</td>
-				</tr>
-			`;
-			template = [templateDirUp, ...template]
-		}
-		tableBody.innerHTML = (template.join(''))
-	}
-
-	const renderInfo = (options, imgSrc = '', isImg = false) => {
-		let templateRender = makeRender('.infoTemplate')
-		let template = options.map((data) => templateRender(data))
-		infoImg.innerHTML = '';
-		infoDownload.innerHTML = '';
-		if ('true' === isImg) {
-			console.log(isImg)
-			let imgTag = document.createElement('img');
-			imgTag.src = imgSrc
-			imgTag.alt = 'image'
-			infoImg.append(imgTag)
-		}
-		console.log(options)
-		if (imgSrc){
-			let downloadLink = document.createElement('a');
-			downloadLink.href = imgSrc
-			downloadLink.download = options[0].value
-			downloadLink.textContent = 'Download'
-			infoDownload.append(downloadLink)
-		}
-		infoUl.innerHTML = template.join('')
-	}
-
+// 	let tableBody = document.querySelector('.table tbody');
+// 	let memoryWrap = document.querySelector('.space');
+// 	let infoUl = document.querySelector('.information-content ul');
+// 	let infoImg = document.querySelector('.information-media');
+// 	let infoDownload = document.querySelector('.information-download');
+// 	let state = [];
+// 	let stateInner = [];
+// 	let idDir;
+//
+// 	const makeRender = (selector) => {
+// 		let template = document.querySelector(selector).innerHTML;
+// 		return new Function('data', 'return `' + template + '`');
+// 	}
+//
+// 	const renderFileSystemMemory = (memory) => {
+// 		memoryWrap.innerHTML = '';
+// 		let listRender = makeRender('.freeSpaceTemplate');
+// 		memoryWrap.innerHTML = listRender(memory);
+// 	}
+//
+// 	const renderTableBody = (userFiles, dir = false, up = false) => {
+// 		tableBody.innerHTML = '';
+// 		let listRender = makeRender('.tableBody');
+// 		let template = userFiles.map((data) => listRender(data))
+// 		if (up) {
+// 			stateInner.slice(0, -1)
+// 		}
+// 		if (dir) {
+// 			let templateDirUp = `
+// 				<tr data-up>
+// 					<td colspan="4">...</td>
+// 				</tr>
+// 			`;
+// 			template = [templateDirUp, ...template]
+// 		}
+// 		tableBody.innerHTML = (template.join(''))
+// 	}
+//
+// 	const renderInfo = (options, imgSrc = '', isImg = false) => {
+// 		let templateRender = makeRender('.infoTemplate')
+// 		let template = options.map((data) => templateRender(data))
+// 		infoImg.innerHTML = '';
+// 		infoDownload.innerHTML = '';
+// 		if ('true' === isImg) {
+// 			console.log(isImg)
+// 			let imgTag = document.createElement('img');
+// 			imgTag.src = imgSrc
+// 			imgTag.alt = 'image'
+// 			infoImg.append(imgTag)
+// 		}
+// 		console.log(options)
+// 		if (imgSrc){
+// 			let downloadLink = document.createElement('a');
+// 			downloadLink.href = imgSrc
+// 			downloadLink.download = options[0].value
+// 			downloadLink.textContent = 'Download'
+// 			infoDownload.append(downloadLink)
+// 		}
+// 		infoUl.innerHTML = template.join('')
+// 	}
+//
 	const getRequest = async (url) => {
-		let res = await fetch(url)
+		let res = await fetch(url, {
+			headers : {
+				'Content-Type': 'text/html',
+			}
+		})
 		if (!res.ok) throw new Error('Error - ' + res.status);
 		const response = await res.json()
 		return await response;
 	}
 
-	const renderDirItems = (idDir) => {
-		let dirItems = stateInner[stateInner.length - 1].filter(obj =>{
-			return  obj.id === +idDir
-		})
-		stateInner.push(dirItems[0].items)
-		renderTableBody(dirItems[0].items, true)
-	}
+// 	const renderDirItems = (idDir) => {
+// 		let dirItems = stateInner[stateInner.length - 1].filter(obj =>{
+// 			return  obj.id === +idDir
+// 		})
+// 		stateInner.push(dirItems[0].items)
+// 		renderTableBody(dirItems[0].items, true)
+// 	}
+//
 
-	getRequest(`http://localhost:3010${username}-file/`).then(({userFiles, memory}) => {
-		renderTableBody(userFiles);
-		state = userFiles;
-		stateInner.push(state);
-		renderFileSystemMemory(memory);
-	})
 
+	// getRequest(`http://localhost:3010/user/admin/?color11=red&color2=blue`)
+
+//
 	table.addEventListener('dblclick',e => {
 		e.stopPropagation()
 		let elem;
 		if (e.target.closest('[data-type="dir"]')) {
 			elem = e.target.closest('[data-type="dir"]');
-			idDir = elem.getAttribute('data-id');
-			renderDirItems(idDir)
-		}
-		if (e.target.closest('[data-up]')) {
-			stateInner = stateInner.slice(0, -1)
-			let dir = true;
-			if (stateInner.length === 1) {
-				dir = false
-			}
-			renderTableBody(stateInner[stateInner.length - 1], dir, true)
-		}
-	})
+			let idDir = elem.getAttribute('data-id');
+			console.log(idDir)
+			// getRequest(`http://localhost:3010/user/admin/?idDir=${idDir}`)
+			getRequest('http://localhost:3010/user/admin/?idDir=uploads/admin/testFolder').then((data)=>{
+				console.log(data)
+			})
 
-	table.addEventListener('click', e => {
-		e.stopPropagation();
-		let elem;
-		if (e.target.closest('[data-type]')) {
-			elem = e.target.closest('[data-type]');
-			let options = [
-				{name: 'Name', value: elem.querySelector('[data-name]').textContent},
-				{name: 'Size', value: elem.querySelector('[data-size]').textContent},
-				{name: 'Created', value: elem.querySelector('[data-created]').textContent},
-			]
-			if (elem.hasAttribute('data-items-length')) {
-				options.push({
-					name: 'Files count',
-					value: elem.getAttribute('data-items-length')
-				})
-			}
-			let path;
-			if (elem.hasAttribute('data-src')) {
-				path = elem.getAttribute('data-src');
-			}
-			let isImg;
-			if (elem.hasAttribute('data-is-img')) {
-				isImg = elem.getAttribute('data-is-img');
-			}
-			renderInfo(options, path, isImg)
 		}
 	})
+//
+// 	table.addEventListener('click', e => {
+// 		e.stopPropagation();
+// 		let elem;
+// 		if (e.target.closest('[data-type]')) {
+// 			elem = e.target.closest('[data-type]');
+// 			let options = [
+// 				{name: 'Name', value: elem.querySelector('[data-name]').textContent},
+// 				{name: 'Size', value: elem.querySelector('[data-size]').textContent},
+// 				{name: 'Created', value: elem.querySelector('[data-created]').textContent},
+// 			]
+// 			if (elem.hasAttribute('data-items-length')) {
+// 				options.push({
+// 					name: 'Files count',
+// 					value: elem.getAttribute('data-items-length')
+// 				})
+// 			}
+// 			let path;
+// 			if (elem.hasAttribute('data-src')) {
+// 				path = elem.getAttribute('data-src');
+// 			}
+// 			let isImg;
+// 			if (elem.hasAttribute('data-is-img')) {
+// 				isImg = elem.getAttribute('data-is-img');
+// 			}
+// 			renderInfo(options, path, isImg)
+// 		}
+// 	})
 })
