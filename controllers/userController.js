69beec98c7f88a9406051cdc987ea9824723fbe4
Dmitriy Pilyavskiy <dmitriy.pilyavskiy@gmail.com>
2021-05-28 13:26:30 +0300

    done add folder and search

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,121 +1,124 @@
 const userModel = require('../models/userModel');
 const fileApp = require('../models/File');
-
+const fs = require('fs')
 
 class UserController {
-    register(req, res) {
-        const {
-            method,
-            body: {username, password},
-        } = req;
+	register(req, res) {
+		const {
+			method,
+			body: {username, password},
+		} = req;
 
-        if (method === 'GET') return res.render('pages/create.hbs');
+		if (method === 'GET') return res.render('pages/create.hbs');
 
-        userModel.register(username, password, result => {
-            const {success, msg} = result;
+		userModel.register(username, password, result => {
+			const {success, msg} = result;
 
-            if (!success) {
-                return res.render('pages/create.hbs', {
-                    erroMessage: msg,
-                });
-            } else {
-                userModel.loginPromised(username, password, result => {
-                    const {success, msg} = result;
-                    if (!success) {
-                        return res.render('pages/login.hbs', {
-                            erroMessage: msg,
-                        });
-                    }
-                    res.cookie('token', msg, {
-                        httpOnly: true,
-                    });
-                    fileApp.createUserDir(username)
-                    res.redirect('/' + username);
-                });
-            }
-        });
-    }
+			if (!success) {
+				return res.render('pages/create.hbs', {
+					erroMessage: msg,
+				});
+			} else {
+				userModel.loginPromised(username, password, result => {
+					const {success, msg} = result;
+					if (!success) {
+						return res.render('pages/login.hbs', {
+							erroMessage: msg,
+						});
+					}
+					res.cookie('token', msg, {
+						httpOnly: true,
+					});
+					res.cookie('username', username);
+					fileApp.createUserDir(username)
+					res.redirect('/user/' + username);
+				});
+			}
+		});
+	}
 
-    login(req, res) {
-        const {username, password} = req.body;
-        const {method} = req;
+	login(req, res) {
+		const {username, password} = req.body;
+		const {method} = req;
 
-        if (method === 'GET') return res.render('pages/login.hbs');
+		if (method === 'GET') return res.render('pages/login.hbs');
 
-        userModel.loginPromised(username, password, result => {
-            const {success, msg} = result;
-            if (!success) {
-                return res.render('pages/login.hbs', {
-                    erroMessage: msg,
-                });
-            }
-            res.cookie('token', msg, {
-                httpOnly: true,
-            });
-            res.redirect('/' + username);
-        });
-    }
+		userModel.loginPromised(username, password, result => {
+			const {success, msg} = result;
+			if (!success) {
+				return res.render('pages/login.hbs', {
+					erroMessage: msg,
+				});
+			}
+			res.cookie('token', msg, {
+				httpOnly: true,
+			});
+			res.cookie('username', username);
+			res.redirect('/user/' + username);
+		});
+	}
 
-    logout(req, res) {
-        res.clearCookie('token');
-        res.redirect('/');
-    }
+	logout(req, res) {
+		res.clearCookie('token');
+		res.clearCookie('username');
+		res.redirect('/');
+	}
 
-    isValidUser(req, res, next) {
-        userModel.isValidToken(req.cookies.token || '', isValid => {
-            const {url} = req;
+	isValidUser(req, res, next) {
+		userModel.isValidToken(req.cookies.token || '', isValid => {
+			const {url} = req;
 
-            if (isValid) next();
-            else if (url !== '/user/login' && url !== '/user/create') {
-                res.redirect('/user/login');
-            } else {
-                next();
-            }
-        });
-    }
+			if (isValid) next();
+			else if (url !== '/user/login' && url !== '/user/create') {
+				res.redirect('/user/login');
+			} else {
+				next();
+			}
+		});
+	}
 
-    getUserItems(req, res) {
-        const {username} = req.params;
-        const {idDir} = req.query;
-        let userFiles;
-        let success = '';
-        if (idDir) {
-            userFiles = fileApp.getFolderItems(idDir, username)
-        } else {
-            userFiles = fileApp.getFolderItems(upload_dir + '/' + username, username)
-        }
-        if (req.method === 'POST') {
-            try {
-                if (!req.files) {
-                    res.json({
-                        success: false,
-                        message: 'No file uploaded',
-                    });
-                } else {
-                    const filesList = Object.values(req.files);
-                    const fileSize = filesList[0].size
-                    console.log((fileApp.UsedMemory + fileSize) < fileApp.AllMemory)
-                    if ((fileApp.UsedMemory + fileSize) < fileApp.AllMemory) {
-                        fileApp.addFiles(filesList, username)
-                        userFiles = fileApp.getFolderItems(upload_dir + '/' + username, username)
-                    } else {
-                        success = 'Not Enough Memory'
-                    }
-                }
-            } catch (err) {
-                res.status(500).json({success: false, message: 'Server error'});
-            }
-        }
-        console.log(userFiles.length)
-        res.render('pages/home.hbs', {
-            success,
-            username: username,
-            userFilesCount: userFiles.length,
-            userFiles: userFiles,
-            memory: fileApp.getMemory(userFiles).usedMemory
-        })
-    }
+	getUserItems(req, res) {
+		const {username} = req.params;
+		let userFiles = fileApp.getFolderItems(upload_dir + '/' + username)
+		let success = '';
 
+		res.render('pages/home.hbs', {
+			success,
+			username: username,
+			userFilesCount: userFiles.length,
+			userFiles: userFiles,
+			memory: fileApp.getMemory(userFiles).freeMemory,
+			allMemory: fileApp.getMemory(userFiles).allMemory
+		})
+	}
+
+	getUserDirItemsJson(req, res) {
+		const {username} = req.params;
+		const {idDir = `uploads/${username}`} = req.query;
+		let allUserFiles = fileApp.getFolderItems(upload_dir + '/' + username)
+		let userFiles
+		if (idDir) {
+			userFiles = fileApp.getFolderItems(idDir)
+			res.json({userFiles, parentDir: idDir, memory: fileApp.getMemory(allUserFiles)})
+		}
+		if (req.method === 'POST') {
+			try {
+				if(req.body.nameDir){
+					fs.mkdirSync(idDir + '/' + req.body.nameDir)
+				}
+				if (req.files) {
+					const filesList = Object.values(req.files);
+					const fileSize = filesList[0].size
+					if ((fileApp.UsedMemory + fileSize) < fileApp.AllMemory) {
+						fileApp.addFiles(filesList, username, idDir)
+					}
+				}
+			} catch (err) {
+				console.log(err)
+				// res.status(500).json({success: false, message: 'Server error'});
+			}
+		}
+	}
 }
 
 exports.UserController = new UserController();
--- public/js/front.js
+++ public/js/front.js
@@ -1,131 +1,226 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const username = window.location.pathname;
 	const table = document.querySelector('.table')
-// 	let tableBody = document.querySelector('.table tbody');
-// 	let memoryWrap = document.querySelector('.space');
-// 	let infoUl = document.querySelector('.information-content ul');
-// 	let infoImg = document.querySelector('.information-media');
-// 	let infoDownload = document.querySelector('.information-download');
-// 	let state = [];
-// 	let stateInner = [];
-// 	let idDir;
-//
-// 	const makeRender = (selector) => {
-// 		let template = document.querySelector(selector).innerHTML;
-// 		return new Function('data', 'return `' + template + '`');
-// 	}
-//
-// 	const renderFileSystemMemory = (memory) => {
-// 		memoryWrap.innerHTML = '';
-// 		let listRender = makeRender('.freeSpaceTemplate');
-// 		memoryWrap.innerHTML = listRender(memory);
-// 	}
-//
-// 	const renderTableBody = (userFiles, dir = false, up = false) => {
-// 		tableBody.innerHTML = '';
-// 		let listRender = makeRender('.tableBody');
-// 		let template = userFiles.map((data) => listRender(data))
-// 		if (up) {
-// 			stateInner.slice(0, -1)
-// 		}
-// 		if (dir) {
-// 			let templateDirUp = `
-// 				<tr data-up>
-// 					<td colspan="4">...</td>
-// 				</tr>
-// 			`;
-// 			template = [templateDirUp, ...template]
-// 		}
-// 		tableBody.innerHTML = (template.join(''))
-// 	}
-//
-// 	const renderInfo = (options, imgSrc = '', isImg = false) => {
-// 		let templateRender = makeRender('.infoTemplate')
-// 		let template = options.map((data) => templateRender(data))
-// 		infoImg.innerHTML = '';
-// 		infoDownload.innerHTML = '';
-// 		if ('true' === isImg) {
-// 			console.log(isImg)
-// 			let imgTag = document.createElement('img');
-// 			imgTag.src = imgSrc
-// 			imgTag.alt = 'image'
-// 			infoImg.append(imgTag)
-// 		}
-// 		console.log(options)
-// 		if (imgSrc){
-// 			let downloadLink = document.createElement('a');
-// 			downloadLink.href = imgSrc
-// 			downloadLink.download = options[0].value
-// 			downloadLink.textContent = 'Download'
-// 			infoDownload.append(downloadLink)
-// 		}
-// 		infoUl.innerHTML = template.join('')
-// 	}
-//
-	const getRequest = async (url) => {
-		let res = await fetch(url, {
-			headers : {
-				'Content-Type': 'text/html',
-			}
-		})
-		if (!res.ok) throw new Error('Error - ' + res.status);
-		const response = await res.json()
-		return await response;
+	const username = getCookie('username');
+	let tableBody = document.querySelector('.table tbody');
+	let memoryWrap = document.querySelector('.space');
+	let infoUl = document.querySelector('.information-content ul');
+	let infoImg = document.querySelector('.information-media');
+	let infoDownload = document.querySelector('.information-download');
+	let stateInner = [];
+	let formUpload = document.getElementById('upload-form')
+	let addFolder = document.getElementById('add-folder')
+	let dirUrl = `uploads/${username}`;
+	let dirName = '';
+	let searchInput =  document.querySelector('.search')
+
+	function getCookie(name) {
+		let matches = document.cookie.match(new RegExp(
+			"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+		))
+		return matches ? decodeURIComponent(matches[1]) : undefined
 	}
 
-// 	const renderDirItems = (idDir) => {
-// 		let dirItems = stateInner[stateInner.length - 1].filter(obj =>{
-// 			return  obj.id === +idDir
-// 		})
-// 		stateInner.push(dirItems[0].items)
-// 		renderTableBody(dirItems[0].items, true)
-// 	}
-//
+	const makeRender = (selector) => {
+		let template = document.querySelector(selector).innerHTML;
+		return new Function('data', 'return `' + template + '`');
+	}
 
+	const renderFileSystemMemory = (memory) => {
+		memoryWrap.innerHTML = '';
+		let listRender = makeRender('.freeSpaceTemplate');
+		memoryWrap.innerHTML = listRender(memory);
+	}
 
-	// getRequest(`http://localhost:3010/user/admin/?color11=red&color2=blue`)
-
-//
-	table.addEventListener('dblclick',e => {
-		e.stopPropagation()
-		let elem;
-		if (e.target.closest('[data-type="dir"]')) {
-			elem = e.target.closest('[data-type="dir"]');
-			let idDir = elem.getAttribute('data-id');
-			console.log(idDir)
-			// getRequest(`http://localhost:3010/user/admin/?idDir=${idDir}`)
-			getRequest('http://localhost:3010/user/admin/?idDir=uploads/admin/testFolder').then((data)=>{
-				console.log(data)
-			})
-
+	const renderTableBody = (userFiles, dir = '', up = false) => {
+		tableBody.innerHTML = '';
+		let listRender = makeRender('.tableBody');
+		let template = userFiles.map((data) => listRender(data))
+		if (up) {
+			stateInner.slice(0, -1)
 		}
-	})
-//
-// 	table.addEventListener('click', e => {
-// 		e.stopPropagation();
-// 		let elem;
-// 		if (e.target.closest('[data-type]')) {
-// 			elem = e.target.closest('[data-type]');
-// 			let options = [
-// 				{name: 'Name', value: elem.querySelector('[data-name]').textContent},
-// 				{name: 'Size', value: elem.querySelector('[data-size]').textContent},
-// 				{name: 'Created', value: elem.querySelector('[data-created]').textContent},
-// 			]
-// 			if (elem.hasAttribute('data-items-length')) {
-// 				options.push({
-// 					name: 'Files count',
-// 					value: elem.getAttribute('data-items-length')
-// 				})
-// 			}
-// 			let path;
-// 			if (elem.hasAttribute('data-src')) {
-// 				path = elem.getAttribute('data-src');
-// 			}
-// 			let isImg;
-// 			if (elem.hasAttribute('data-is-img')) {
-// 				isImg = elem.getAttribute('data-is-img');
-// 			}
-// 			renderInfo(options, path, isImg)
-// 		}
-// 	})
+		if (dir.length && dir !== `uploads/${username}` && dir !== `uploads/${username}/`) {
+			let templateDirUp = `
+				<tr data-up data-dir="${dir}">
+					<td colspan="4">...</td>
+				</tr>
+			`;
+			template = [templateDirUp, ...template]
+		}
+		tableBody.innerHTML = (template.join(''))
+		console.log('renderTableBody')
+		showSearch();
+	}
+
+	const renderInfo = (options, imgSrc = '', isImg = false) => {
+		let templateRender = makeRender('.infoTemplate')
+		let template = options.map((data) => templateRender(data))
+		infoImg.innerHTML = '';
+		infoDownload.innerHTML = '';
+		if ('true' === isImg) {
+			let imgTag = document.createElement('img');
+			imgTag.src = imgSrc
+			imgTag.alt = 'image'
+			infoImg.append(imgTag)
+		}
+		if (imgSrc) {
+			let downloadLink = document.createElement('a');
+			downloadLink.href = imgSrc
+			downloadLink.download = options[0].value
+			downloadLink.textContent = 'Download'
+			infoDownload.append(downloadLink)
+		}
+		infoUl.innerHTML = template.join('')
+	}
+
+	const getRequest = async (url) => {
+		let res = await fetch(url)
+		if (!res.ok) throw new Error('Error - ' + res.status);
+		return await res.json();
+	}
+
+	function postRequest(url, data) {
+		// const res = await fetch(`${url}`, {
+		// 	method: 'post',
+		// 	headers: {
+		// 	    'Content-type': 'application/json; charset=utf-8'
+		// 	},
+		// 	body: JSON.stringify(data)
+		// 	// body: data
+		// });
+		// console.log(data)
+		// if (!res.ok) {
+		// 	throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+		// }
+		// return await res.json();
+		// return await res.text();
+
+
+	}
+
+	if (table) {
+		table.addEventListener('dblclick', e => {
+			e.stopPropagation()
+			let elem;
+			if (e.target.closest('[data-type="dir"]')) {
+				elem = e.target.closest('[data-type="dir"]');
+				dirUrl = elem.getAttribute('data-dir');
+				dirName = elem.querySelector('[data-name]').textContent;
+				dirUrl = elem.getAttribute('data-dir') + '/' + elem.querySelector('[data-name]').textContent
+				getRequest(`http://localhost:3010/user/${username}-file/?idDir=${dirUrl}`).
+				then(({userFiles, memory, parentDir
+				}) => {
+					renderTableBody(userFiles, parentDir);
+					renderFileSystemMemory(memory);
+				})
+			}
+			if (e.target.closest('[data-up]')) {
+				elem = e.target.closest('[data-up]');
+				let parentUrl = elem.getAttribute('data-dir');
+				dirUrl = parentUrl.slice(0, dirUrl.lastIndexOf('/'))
+				getRequest(`http://localhost:3010/user/${username}-file/?idDir=${dirUrl}`)
+					.then(({userFiles, memory, parentDir
+				}) => {
+					renderTableBody(userFiles, parentDir);
+					renderFileSystemMemory(memory);
+				})
+			}
+		})
+
+		table.addEventListener('click', e => {
+			e.stopPropagation();
+			let elem;
+			if (e.target.closest('[data-type]')) {
+				elem = e.target.closest('[data-type]');
+				let options = [
+					{name: 'Name', value: elem.querySelector('[data-name]').textContent},
+					{name: 'Size', value: elem.querySelector('[data-size]').textContent},
+					{name: 'Created', value: elem.querySelector('[data-created]').textContent},
+				]
+				if (elem.hasAttribute('data-items-length')) {
+					options.push({
+						name: 'Files count',
+						value: elem.getAttribute('data-items-length')
+					})
+				}
+				let path;
+				if (elem.hasAttribute('data-src')) {
+					path = elem.getAttribute('data-src');
+				}
+				let isImg;
+				if (elem.hasAttribute('data-is-img')) {
+					isImg = elem.getAttribute('data-is-img');
+				}
+				renderInfo(options, path, isImg)
+			}
+		})
+	}
+
+	if (addFolder) {
+		addFolder.addEventListener('submit', (e) => {
+			e.preventDefault();
+			let input = addFolder.querySelector('input')
+			let nameDir = input.value;
+			const formData = new FormData()
+			formData.append('nameDir', nameDir)
+			fetch(`http://localhost:3010/user/${username}-file/?idDir=${dirUrl}`, {
+				method: 'POST',
+				body: formData
+			}).then(data => {
+				input.value = ''
+				getRequest(`http://localhost:3010/user/${username}-file/?idDir=${dirUrl}`).
+				then(({userFiles, memory, parentDir}) => {
+					renderTableBody(userFiles, parentDir);
+					renderFileSystemMemory(memory);
+				})
+			})
+		})
+	}
+
+	function displayRows (input, rows) {
+		let searchValue = input.value.toLowerCase().trimLeft();
+		console.log(searchValue)
+		rows.forEach( row => {
+			[...row.querySelectorAll('[data-name]')]
+				.some( value => value.textContent.toLowerCase().trimLeft().indexOf(searchValue) > -1 )
+				? row.style.display = 'table-row'
+				: row.style.display = 'none'
+		} )
+	}
+
+	const showSearch = () => {
+		let rows = document.querySelectorAll('tbody tr');
+		if (searchInput.value.toLowerCase().length > 0) displayRows(searchInput, rows);
+		searchInput.addEventListener('keyup', () => {
+			console.log(1111)
+			displayRows(searchInput, rows)
+		})
+	}
+
+	if (formUpload) {
+		formUpload.addEventListener('submit', (e) => {
+			e.preventDefault();
+			let input = formUpload.querySelector('[type="file"]')
+			let file = input.files[0]
+			const formData = new FormData()
+			formData.append('myFile', file)
+			fetch(`http://localhost:3010/user/${username}-file/?idDir=${dirUrl}`, {
+				method: 'POST',
+				body: formData
+			})
+				.then(response => response.json())
+				.then(data => {
+					input.value = ''
+					getRequest(`http://localhost:3010/user/${username}-file/?idDir=${dirUrl}`).
+					then(({userFiles, memory, parentDir}) => {
+						renderTableBody(userFiles, parentDir);
+						renderFileSystemMemory(memory);
+					})
+				})
+				.catch(error => {
+					console.error(error)
+				})
+		})
+	}
+
+	showSearch();
 })
