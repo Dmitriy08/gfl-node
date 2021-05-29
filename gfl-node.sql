3c33b942e5bc22cb45dd5ba05d87dc64c57a60df
Dmitriy Pilyavskiy <dmitriy.pilyavskiy@gmail.com>
2021-05-29 11:50:22 +0300

    add DB

--- gfl-node.sql
+++ gfl-node.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5ubuntu0.5
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost:3306
--- Время создания: Май 28 2021 г., 18:27
--- Версия сервера: 5.7.34-0ubuntu0.18.04.1
--- Версия PHP: 7.2.34-21+ubuntu18.04.1+deb.sury.org+1
+-- Время создания: Май 29 2021 г., 11:49
+-- Версия сервера: 8.0.25-0ubuntu0.20.04.1
+-- Версия PHP: 7.3.28-1+ubuntu20.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -27,18 +29,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `id` int NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `password` varchar(50) COLLATE utf8_bin NOT NULL,
   `token` varchar(100) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
 
 --
 -- Дамп данных таблицы `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `password`, `token`) VALUES
-(17, 'dchaika', '5b655bd7eb774375adf2d15df7b15b5e7dd37289', '34b4fbaaf1347ce1f03696cee5c269d9d4be37f4');
+(3, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', '3645ea637da9e98565c73da635fdb5d6f5e42100'),
+(4, 'Dmitriy', 'b6638a70f575ee00024a857d86e738db52ca0a01', '41cbba435c9050b11cf25dd114d5368558f9b400');
 
 --
 -- Индексы сохранённых таблиц
@@ -58,7 +61,9 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
